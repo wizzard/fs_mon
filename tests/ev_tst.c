1609d1f25ebe2a8a6bfb7ded827451676f3cdd52
Paul Ionkin <paul.ionkin@gmail.com>
2013-09-21 18:00:32 +0200

    fix

--- tests/ev_tst.c
+++ tests/ev_tst.c
@@ -7,12 +7,40 @@
 #include <event2/http.h>
 #include <event2/http_struct.h>
 
-int main ()
+static void on_read (int fd, short event, void *arg)
 {
+    char buf[255];
+    int len;
+
+    printf ("On read !\n");
+
+	len = read (fd, buf, sizeof(buf) - 1);
+
+    printf ("Read: %d bytes\n", len);
+
+}
+
+int main (int argc, char *argv[])
+{
+    int fd;
     struct event_base *evbase;
+    struct event *ev;
 
     evbase = event_base_new ();
-
     printf ("method: %s\n", event_base_get_method (evbase));
+
+    fd = open (argv[1], O_RDONLY);
+    if (fd < 0) {
+        printf ("Failed to open !\n");
+        return -1;
+    }
+    ev = event_new (evbase, fd, EV_READ|EV_PERSIST, on_read, NULL);
+    if (!ev) {
+        printf ("Failed to create event !\n");
+        return -1;
+    }
+
+    event_base_dispatch (evbase);
+
     return 0;
 }
