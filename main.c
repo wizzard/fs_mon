0e1fadeedbd5396741c2d7266da1b40c8fe845d9
Paul Ionkin <paul.ionkin@gmail.com>
2013-09-22 04:57:19 +0200

    fix

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+#define _GNU_SOURCE 1
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/resource.h>
@@ -26,19 +27,74 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
-#include <fam.h>
+#include <glib.h>
+#include <glib/gprintf.h>
+#include <gio/gio.h>
 
 #include "khash.h"
 
 static long unsigned int total_handlers = 0;
 KHASH_MAP_INIT_INT(h32, char*)
 static khash_t(h32) *h;
-static FAMConnection fc;
 static int global_wd = 0;
 
 #define REPORT_EACH 1000
 
 static int fd;
+
+int scan_dir (const char *dir_path);
+
+static void
+dir_changed_cb (GFileMonitor      *monitor,
+            GFile             *file,
+            GFile             *other_file,
+            GFileMonitorEvent  event,
+            gpointer           data)
+{
+    switch (event) {
+        case G_FILE_MONITOR_EVENT_CHANGED:
+            g_printf ("Changed: %s \n", g_file_get_path (file));
+            break;
+        case G_FILE_MONITOR_EVENT_DELETED:
+            g_printf ("event deleted: %s \n", g_file_get_path (file));
+            break;
+        case G_FILE_MONITOR_EVENT_CREATED:
+            g_printf ("event created: %s \n", g_file_get_path (file));
+            scan_dir (g_file_get_path (file));
+            break;
+        case G_FILE_MONITOR_EVENT_ATTRIBUTE_CHANGED:
+            g_printf ("event changed: %s \n", g_file_get_path (file));
+            break;
+
+        case G_FILE_MONITOR_EVENT_MOVED:
+            g_printf ("event moved: %s  old: %s\n", g_file_get_path (file), g_file_get_path (other_file));
+            break;
+
+    }
+}
+
+static int install_watch (const char *dir_path)
+{
+    GFile *file;
+    GError *error = NULL;
+    GFileMonitor *dir_monitor;
+
+    file = g_file_new_for_path (dir_path);
+    if (!file) {
+        g_printf ("Failed to create file object from path: %s\n", dir_path);
+        return -1;
+    }
+
+    dir_monitor = g_file_monitor_directory (file, 0, NULL, &error);
+    if (!dir_monitor) {
+        g_printf ("Failed to create directory monitor for: %s\n", dir_path);
+        return -1;
+    }
+
+    g_signal_connect (dir_monitor, "changed", G_CALLBACK (dir_changed_cb), NULL);
+    
+    return 0;
+}
 
 // 0: ok
 // 1: error
@@ -49,10 +105,16 @@ int scan_dir (const char *dir_path)
     static struct stat st;
     char *cur_path;
     char *next_file;
-    int wd;
     khiter_t k;
     int ret;
-    FAMRequest fr;
+
+    if (lstat (dir_path, &st) == -1) {
+        fprintf (stderr, "Failed to stat file: %s\n", dir_path);
+        return -1;
+    }
+    
+    if (!S_ISDIR (st.st_mode))
+        return 0;
 
     dir = opendir (dir_path);
     if (!dir) {
@@ -65,16 +127,14 @@ int scan_dir (const char *dir_path)
 	} else {
 		cur_path = (char *)dir_path;
 	}
-    global_wd++;
-    wd = global_wd;
 
-    ret = FAMMonitorDirectory (&fc, cur_path, &fr, (void *)wd);
+    ret = install_watch (cur_path);
     if (ret < 0) {
         fprintf (stderr, "add_watch failed. So far: %lu watches added \n", total_handlers);
         return -1;
     }
     total_handlers++;
-    
+    /*
     k = kh_put(h32, h, wd, &ret);
     if (!ret) {
         fprintf (stderr, "Hash error !\n");
@@ -82,9 +142,9 @@ int scan_dir (const char *dir_path)
     }
 
     kh_value(h, k) = strdup (cur_path);
-    
+    **/
     if (total_handlers % REPORT_EACH == 0) {
-        fprintf (stderr, "So far: %lu watches added, current wd: %d\n", total_handlers, wd);
+        fprintf (stderr, "So far: %lu watches added\n", total_handlers);
     }
 
     ent = readdir (dir);
@@ -117,50 +177,10 @@ int scan_dir (const char *dir_path)
     return 0;
 }
 
-void process_events (FAMEvent* fe)
-{
-    if (fe->code == FAMCreated) {
-        char *parent = NULL;
-        khiter_t k;
-        int wd;
-
-        wd = (int) fe->userdata;
-        k = kh_get(h32, h, wd);
-
-        if (k != kh_end(h)) {
-            char *fname;
-            static struct stat st;
-
-            parent = kh_value(h, k);
-            if (!parent)
-                fprintf (stderr, "OPS !!\n");
-
-
-            asprintf (&fname,"%s%s", parent, fe->filename);
-
-            if (lstat (fname, &st) != -1) {
-
-                if (S_ISDIR (st.st_mode) && !S_ISLNK (st.st_mode)) {
-
-                    fprintf (stderr, "created in watched directory: wd: %d [%s] parent: %s\n", wd, fe->filename, parent);
-                    scan_dir (fname);
-                }
-
-            } else {
-                fprintf (stderr, "Failed to stat file: %s\n", fname);
-            }
-        } else {
-            fprintf (stderr, "OPS, parent not found ! wd: %d created in watched directory: [%s]\n", wd, fe->filename);
-
-        }
-    }
-}
-
 int main (int argc, char *argv[])
 {
     char target[FILENAME_MAX];
-    int fd;
-    FAMEvent fe;
+    GMainLoop *loop;
 
     if (argc < 2) {
         fprintf (stderr, "Watching the current directory\n");
@@ -172,12 +192,6 @@ int main (int argc, char *argv[])
 
     h = kh_init(h32);
 
-    fd = FAMOpen2 (&fc, argv[0]);
-    if (fd < 0) {
-        fprintf (stderr, "FAM init failed\n");
-        return 1;
-    }
-
     if (scan_dir (argv[1]) < 0) {
         return 1;
     }
@@ -185,18 +199,8 @@ int main (int argc, char *argv[])
     fprintf (stderr, "Total: %lu watches \n", total_handlers);
     fflush (stderr);
 
-    while (1) {
-        if (FAMPending (&fc) < 0) {
-            printf (stderr, "FAMPending() returned < 0!\n");
-            exit (1);
-        }
-        if (FAMNextEvent (&fc, &fe) < 0) {
-            printf (stderr, "FAMNextEvent() returned < 0!\n");
-            exit (1);
-        }
-	    process_events (&fe);
-	}
-
+    loop = g_main_loop_new (NULL, FALSE);
+    g_main_loop_run (loop);
 
     kh_destroy(h32, h);
 
